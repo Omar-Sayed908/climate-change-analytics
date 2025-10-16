1c6795a0cd14f772447bed631837b1525fa51f6b
Doaa Ali <doali246885@gmail.com>
2025-10-16 22:24:01 +0300

    Load all Dims in SSIS

--- SQL_Queries/climate changes.sql
+++ SQL_Queries/climate changes.sql
@@ -2,25 +2,27 @@ create database Climate
 GO
 USE Climate
 GO
-create table source
-(source_id int primary key,
-source_name varchar(50),
-source_url varchar(200),
-created_at date default getdate() ,
-updated_at date default getdate()
 
-)
+create table source (
+    source_id int primary key IDENTITY(1, 1),
+    source_name varchar(50),
+    source_url varchar(200),
+    created_at date default getdate(),
+    updated_at date default getdate()
+);
+
 CREATE TABLE Indicator (
-    Indicator_ID INT PRIMARY KEY,
+    Indicator_ID INT PRIMARY KEY IDENTITY(1, 1),
     Category VARCHAR(100),
     Indicator_Name VARCHAR(100),
     Description TEXT,
     Created_At DATE default getdate(),
     Updated_At DATE default getdate(),
-    Unit VARCHAR(20),
+    Unit VARCHAR(50),
     S_ID INT,
-    FOREIGN KEY (S_ID) REFERENCES Source(Source_ID) ON DELETE CASCADE
+    FOREIGN KEY (S_ID) REFERENCES Source(Source_ID) ON DELETE SET NULL
 );
+
 
 -- Country Table
 CREATE TABLE Country (
@@ -33,13 +35,17 @@ CREATE TABLE Country (
 
 -- Observation Table
 CREATE TABLE Observation (
-    Observation_ID INT PRIMARY KEY,
+    Observation_ID INT PRIMARY KEY IDENTITY(1, 1),
     Value FLOAT,
     Year INT,
     Created_At DATE default getdate(),
     Updated_At DATE default getdate(),
     I_ID INT,
     C_ID INT,
-    FOREIGN KEY (I_ID) REFERENCES Indicator(Indicator_ID) ON DELETE CASCADE,
-    FOREIGN KEY (C_ID) REFERENCES Country(Country_ID) ON DELETE CASCADE
+    FOREIGN KEY (I_ID) REFERENCES Indicator(Indicator_ID) ON DELETE SET NULL,
+    FOREIGN KEY (C_ID) REFERENCES Country(Country_ID) ON DELETE SET NULL
 );
+
+
+-- DROP DATABASE Climate
+
